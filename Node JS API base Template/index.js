a66075e3a9d7578f986fb04e3a36e8554d65fde5
mayurmarvel <45968855+mayurmarvel@users.noreply.github.com>
2022-07-11 14:03:48 +0530

    Rest Api

---
+++ Node JS API base Template/index.js
@@ -0,0 +1,40 @@
+const express =  require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const PORT = process.env.PORT || 3000;
+
+
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json()); //accepting only json data
+
+
+app.get('/', (req,res)=>{
+    res.json({
+        message: 'Welcome to My Api'
+    })
+
+
+}) 
+
+app.post('/post', (req,res)=>{
+
+    // console.log(req.params);
+    res.send("Received")
+
+
+}) 
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Listening at http://localhost:${PORT}`);
+})
+
