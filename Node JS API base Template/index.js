8803a2ace22e6baa77dcecd9dad2c566def89288
mayurmarvel <45968855+mayurmarvel@users.noreply.github.com>
2022-07-11 14:32:10 +0530

    Rest Api

--- Node JS API base Template/index.js
+++ Node JS API base Template/index.js
@@ -2,9 +2,10 @@ const express =  require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const { nextTick } = require('process');
+const { error } = require('console');
 
 const PORT = process.env.PORT || 3000;
-
 
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json()); //accepting only json data
+// app.use(express.urlencoded());  //accept url encoded data
 
 
 app.get('/', (req,res)=>{
@@ -23,14 +25,36 @@ app.get('/', (req,res)=>{
 
 }) 
 
-app.post('/post', (req,res)=>{
+app.post('/post', (req,res,next)=>{
 
-    // console.log(req.body);
-    res.send("Received")
+    console.log(req.body);
+
+    try{
+
+        res.send("Received")
 
 
+    }catch(error){
+
+    next(error);
+
+    }
+
+    
 }) 
 
+
+app.use((error,req,res,next) => {
+    if(error.status){
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: error.stack
+    })
+})
 
 
 
